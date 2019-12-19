64b12858fa1d7f15a8afafce24333cb11212ea04
akatoybits <DIMACUAS@legal.regn.net>
2019-12-20 07:58:45 +0800

    created initial file

---
+++ test.js
@@ -0,0 +1,10 @@
+var a = 2;
+var b = 2;
+var c = a + b;
+for (i = 0; i <=10000; i++)
+{
+console.log("This is my first node js code: " + Math.random(10000));	
+}
+
+console.log(c);
+console.log("This is my first node js code");
